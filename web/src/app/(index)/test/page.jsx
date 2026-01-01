a0e612298df7ffc0403257c8be70a7dbeb81de0f
mo-blog <7969803@qq.com>
2026-01-01 19:19:34 +0800

    简化 test 页面

--- web/src/app/(index)/test/page.jsx
+++ web/src/app/(index)/test/page.jsx
@@ -1,22 +1,10 @@
-
-
-
-
-
-
-export default function Page() {
-
-
-
-    return (
-        <div className="">
-
-            {/*<div  className="w-full h-[200px]">*/}
-            {/*    <Banner title="test" imageUrl=""/>*/}
-            {/*</div>*/}
-
-
-
-        </div>
-    )
+export default function TestPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold mb-4">测试页面</h1>
+      <p className="text-gray-600">
+        如果你能看到这个页面，说明基础功能正常
+      </p>
+    </div>
+  );
 }
